ebeca8e0469f04caab2a674aefa62462f022b19d
caizehui <905718992@qq.com>
2025-02-24 00:30:53 +0800

    update

--- course/py/base.py
+++
@@ -1,8 +0,0 @@
-# coding=utf-8
-
-# 导入模块
-import sys
-import os
-
-if __name__ == "__main__":
-    print("Hello World!")
--- course/py/helloworld.py
+++
@@ -1,5 +0,0 @@
-# coding=utf-8
-
-# 入口函数
-if __name__ == "__main__":
-    print("Hello World!")
